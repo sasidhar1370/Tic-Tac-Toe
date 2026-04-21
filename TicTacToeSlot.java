fa752691ab78fd4b2302637e29acb9d3530264ee
sasidhar1370 <sasidhar1370@gmail.com>
2026-04-21 15:09:20 +0530

    uc3 done

---
+++ TicTacToeSlot.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class TicTacToeSlot {
+    public static void main(String[] args) {
+        int slot = getUserSlot();
+        System.out.println("Slot entered: " + slot);
+    }
+  static int getUserSlot() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your slot selection (1-9): ");
+        int slot = sc.nextInt();
+        return slot;
+    }
+}
